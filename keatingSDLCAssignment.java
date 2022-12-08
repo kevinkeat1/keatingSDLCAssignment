4447c8f3e0bbcc5d332dc601a09879f850d264ee
kevinkeat1 <112182154+kevinkeat1@users.noreply.github.com>
2022-12-08 11:54:20 -0500

    Add files via upload

--- keatingSDLCAssignment.java
+++ keatingSDLCAssignment.java
@@ -5,17 +5,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.nio.file.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.regex.*;
+import java.util.stream.Stream;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.util.Map.*;
+import java.util.logging.Logger;
 
+/**
+ * 
+ * @author Kevin
+ *
+ */
 public class keatingSDLCAssignment {
 	
 	static TreeMap<String, Integer> freq = new TreeMap<>();
-	static Map<String, Integer> sortedMap = valueSort(freq);
+	private static DefaultListModel<String> textFeild;
+	static JTable myTable = new JTable(); 
+	private static DefaultTableModel defaultModel;
+	private static List<String> data = null;
+	private static final String fileName = "theRaven.txt";
 	
+	/**
+	 * This method sorts the Map
+	 * @param <K>
+	 * K is each word of the Map
+	 * @param <V>
+	 * V is the number of times each string of K
+	 * occurs
+	 * @param map
+	 * Map that will be displayed
+	 * @return
+	 * Returns a sorted Map that will be displayed
+	 */
 	 public static <K, V extends Comparable<V> > Map<K, V> //Sorts the TreeMap
 	    valueSort(final Map<K, V> map)
 	    {
@@ -39,6 +67,9 @@ public class keatingSDLCAssignment {
 	        return sorted;
 	    }
 	 
+	 /**
+	  * Constructs the gui and is where data is displayed
+	  */
 	 public void init() {
 		 JFrame frame = new JFrame(); //Initial panel, will have button that when clicked will display values
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,37 +80,44 @@ public class keatingSDLCAssignment {
 			JPanel occurances = new JPanel();
 			
 			String[] columns = new String[] {"Word", "Number"}; //So I can put treemap into the UI
-			DefaultTableModel defaultModel = new DefaultTableModel(columns, 0);
+			defaultModel = new DefaultTableModel(columns, 0);
 			
-			Set<Entry<String, Integer>> entries = sortedMap.entrySet();
 			
-			for(Entry<String, Integer> entry:entries) {
-				defaultModel.addRow(new Object[] {entry.getKey(), entry.getValue()});
-			}
 			
-			JTable myTable = new JTable(defaultModel);
-			JScrollPane scrollPane = new JScrollPane();
+			myTable = new JTable(defaultModel); //Will be where the data goes
+			JScrollPane scrollPane = new JScrollPane(); //User can scroll through the list
 			scrollPane.setViewportView(myTable);
 			occurances.add(scrollPane);
 			frame.add(occurances);
 			
-			JPanel buttonPanel = new JPanel();
+			JPanel buttonPanel = new JPanel(); //Click this and data will fill into table
 			buttonPanel.add(new JLabel());
 			JButton display = new JButton("Display words");
-			display.addActionListener(displayListener());
+			display.addActionListener(displayData());
 			display.setPreferredSize(new Dimension(140, 30));
 			buttonPanel.add(display);
+			
+			JButton transmit = new JButton("Send data");
+			transmit.addActionListener(transmitData());
+			transmit.setPreferredSize(new Dimension(140, 30));
+			buttonPanel.add(transmit);
 			frame.add(buttonPanel);
 					
 			frame.setVisible(true);
 	 }
 	 
-	 private static ActionListener displayListener(){ //Gets clicked and data is sent
+	 private static ActionListener displayData(){ //Gets clicked and data is sent
 		 return new ActionListener() {
 			 @Override
 				public void actionPerformed(ActionEvent e) {
+				 int rowCount = defaultModel.getRowCount();
+				 System.out.println(rowCount);
+				 for (int i = rowCount - 1; i >= 0; i--) {
+					    defaultModel.removeRow(i);
+					}
 				 freq.clear(); //Will add together each time button is clicked otherwise
-				 Path path = Paths.get("C:\\Users\\kevin\\eclipse-workspace\\keatingSDLCProject\\theRaven.txt"); // Gets file
+				 
+				 Path path = Paths.get(fileName); // Gets file
 			        try {
 			        	
 			            String text = Files.readString(path); // throws java.io.IOException
@@ -101,6 +139,7 @@ public class keatingSDLCAssignment {
 			                    freq.computeIfAbsent(word, (w) -> Integer.valueOf(1));
 			                }
 			            }
+			    		
 			            // Calling the method valueSort
 			            Map sortedMap = valueSort(freq);
 			      
@@ -112,11 +151,12 @@ public class keatingSDLCAssignment {
 			      
 			            // Display elements
 			           while (i.hasNext()) {
-			                
-			                Map.Entry mp = (Map.Entry)i.next();
-			                System.out.print(mp.getKey() + ": ");
-			                System.out.println(mp.getValue());
-			                
+			        	   Set<Entry<String, Integer>> entries = sortedMap.entrySet();
+			   			
+			   			for(Entry<String, Integer> entry:entries) {
+			   				defaultModel.addRow(new Object[] {entry.getKey(), entry.getValue()});
+			   			}
+			                Map.Entry mp = (Map.Entry)i.next(); //Will run out of memory otherwise
 			            }
 			        }
 			        catch (IOException xIo) {
@@ -125,10 +165,85 @@ public class keatingSDLCAssignment {
 			 }
 		 };
 	 }
-
-	public static void main(String[] args) throws FileNotFoundException {
-		
+	 
+	 private static ActionListener transmitData() { //Sends data to mySQL database, added for a little bit of flaire
+		 return new ActionListener() {
+			 @Override
+				public void actionPerformed(ActionEvent e) {
+				 try {
+					getConnection();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			 }
+		 };
+	 }
+	
+	 /**
+	  * 
+	  * @param args
+	  * For main method
+	 * @throws Exception 
+	  */
+	public static void main(String[] args) throws Exception {
+		/**
+		 * This is the start of the main method
+		 * 
+		 * code execution
+		 */
 		new keatingSDLCAssignment().init();
 	}
+	
+	public static Connection getConnection() throws Exception{
+		class DBConnection {
 
+			final private String url = "jdbc:mysql://localhost/";
+			final private String databaseName = "wordOccurances";
+			final private String user = "root";
+			final private String password = "cop2805";
+
+			public Connection Connect() {
+			    Connection c = null;
+			    try {
+			        c = DriverManager.getConnection(url + databaseName, user, password);
+			    } catch (SQLException ex) {
+			        
+			    }
+			    return c;
+			}}
+		
+		Connection con = new DBConnection().Connect();
+		String query = "INSERT INTO word (word) VALUES (?)";
+		PreparedStatement stmt;
+		try {
+			stmt = con.prepareStatement(query);
+			List<String> array = new ArrayList<String>();
+			Scanner scnr = new Scanner(new FileReader(fileName));
+			String str;
+			
+			while(scnr.hasNext()) {
+				str = scnr.next();
+				String noPunct = str.toLowerCase().replaceAll("[^\\s\\w]", "");
+				array.add(noPunct);
+			}
+			
+			String[] data = array.toArray(new String[0]);
+			
+			for(String words:data) {
+				stmt.setString(1, words);
+				stmt.execute();
+			}
+			
+			stmt.close();
+	        con.close();
+			
+			System.out.println("Connected");
+			
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		return con;
+		
+	}
 }
